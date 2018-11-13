d6f7d9ff3b9807845d9f1131b24e8a3d4758221c
David <44362463+Gencid@users.noreply.github.com>
2018-11-13 02:47:15 +0100

    Using Keys and a Data file.
    For this change I'm importing the Keys library for a more compact code (using TAB instead of finding every box), and created a separate Data file for all the user information.

--- Mer_Tests.py
+++ Mer_Tests.py
@@ -3,6 +3,7 @@ from Pages.MercuryWelcome import *
 from Pages.MercuryRegister import *
 from Pages.CreateAccountSuccess import *
 from Pages.MercurySignOn import *
+from Mer_Data import *
 
 import unittest
 
@@ -17,18 +18,18 @@ class MerToursTests(unittest.TestCase):
         self.signon = SignOn(self.driver)
 
     def test_index_login(self):
-        self.welcome.welcome_login("Usuario", "contraseña")
+        self.welcome.welcome_login(d_username, d_password)
+        self.driver.implicitly_wait(3)
         self.welcome.welcome_go_register()
 
     def test_register(self):
         self.welcome.welcome_go_register()
-        self.register.contact_info("Fulanito", "de Tal", "555 55 55", "prueba@testing.com")
-        self.register.mailing_info("251 Something Street", "Apartment T, Second Floor",
-                                   "Testville", "Testington", "73575", 23)
-        self.register.user_info("Usuario", "contraseña")
+        self.register.contact_info(d_name, d_lastname, d_phone, d_email)
+        self.register.mailing_info(d_dir1, d_dir2, d_city, d_state, d_postcode, d_country)
+        self.register.user_info(d_username, d_password)
         self.register.go_submit()
         self.success.go_sign_in()
-        self.signon.signon_login("Usuario", "contraseña")
+        self.signon.signon_login(d_username, d_password)
 
     def tearDown(self):
         self.driver.quit()
--- Pages/MercuryRegister.py
+++ Pages/MercuryRegister.py
@@ -1,44 +1,31 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+
 
 class Register:
     def __init__(self, mercurydriver):
         self.driver = mercurydriver
         self.signon_link = (By.LINK_TEXT, "SIGN-ON")
         self.register_link = (By.LINK_TEXT, "REGISTER")
-        self.lastname_box = (By.NAME, "lastName")
         self.firstname_box = (By.NAME, "firstName")
-        self.phone_box = (By.NAME, "phone")
-        self.email_box = (By.ID, "userName")
         self.address_box1 = (By.NAME, "address1")
-        self.address_box2 = (By.NAME, "address2")
-        self.city_box = (By.NAME, "city")
-        self.state_box = (By.NAME, "state")
-        self.postcode_box = (By.NAME, "postalCode")
         self.country_dropdown = (By.NAME, "country")
         self.username_box = (By.ID, "email")
-        self.password_box = (By.NAME, "password")
-        self.confirmpass_box = (By.NAME, "confirmPassword")
         self.submit_button = (By.NAME, "register")
 
     def contact_info(self, firstname, lastname, phone, email):
-        self.driver.find_element(*self.lastname_box).send_keys(lastname)
-        self.driver.find_element(*self.firstname_box).send_keys(firstname)
-        self.driver.find_element(*self.phone_box).send_keys(phone)
-        self.driver.find_element(*self.email_box).send_keys(email)
+        self.driver.find_element(*self.firstname_box)\
+            .send_keys(firstname+Keys.TAB+lastname+Keys.TAB+phone+Keys.TAB+email)
 
     def mailing_info(self, address1, address2, city, state, postcode, country):
-        self.driver.find_element(*self.address_box1).send_keys(address1)
-        self.driver.find_element(*self.address_box2).send_keys(address2)
-        self.driver.find_element(*self.city_box).send_keys(city)
-        self.driver.find_element(*self.state_box).send_keys(state)
-        self.driver.find_element(*self.postcode_box).send_keys(postcode)
+        self.driver.find_element(*self.address_box1)\
+            .send_keys(address1+Keys.TAB+address2+Keys.TAB+city+Keys.TAB+state+Keys.TAB+postcode)
         Select(self.driver.find_element(*self.country_dropdown)).select_by_index(country)
 
     def user_info(self, username, password):
-        self.driver.find_element(*self.username_box).send_keys(username)
-        self.driver.find_element(*self.password_box).send_keys(password)
-        self.driver.find_element(*self.confirmpass_box).send_keys(password)
+        self.driver.find_element(*self.username_box)\
+            .send_keys(username+Keys.TAB+password+Keys.TAB+password)
 
     def go_submit(self):
         self.driver.find_element(*self.submit_button).click()
